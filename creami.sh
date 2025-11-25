b0c44e1445b7dce1a0c5ee5783219108ab8fdfa0
almuhdilkarim <al.muhdil.0@gmail.com>
2025-11-26 00:28:31 +0700

    update

--- creami.sh
+++ creami.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 pacman -Syy git --noconfirm &&
+
+if [[ -d "/install" ]];then
+    rm -fr /install
+fi
+
 git clone https://github.com/linux-creami/install.git /install &&
 /bin/bash /install/prep.sh
