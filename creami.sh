478db8940fe934b6773bd9f17b98d7cd688db25d
almuhdilkarim <al.muhdil.0@gmail.com>
2025-11-26 00:59:39 +0700

    update

--- creami.sh
+++ creami.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+
+clear &&
+echo "                                                       /yy"
+echo "                                                      |__/"
+echo "  /yyyyyyy  /yyyyyy   /yyyyyy   /yyyyyy  /yyyyyy/yyyy  /yy"
+echo " /yy_____/ /yy__  yy /yy__  yy |____  yy| yy_  yy_  yy| yy"
+echo "| yy      | yy  \__/| yyyyyyyy  /yyyyyyy| yy \ yy \ yy| yy"
+echo "| yy      | yy      | yy_____/ /yy__  yy| yy | yy | yy| yy"
+echo "|  yyyyyyy| yy      |  yyyyyyy|  yyyyyyy| yy | yy | yy| yy"
+echo " \_______/|__/       \_______/ \_______/|__/ |__/ |__/|__/"
+echo ""
+echo "+------------+--------------------------------------------------+"
+echo "|                  LINUX EYERISE INSTALATION                    |"
+echo "+------------+--------------------------------------------------+"
+echo "| Author     |  Al Muhdil Karim                                 |"
+echo "| Mainteners |  Ibnu Dzaky                                      |"
+echo "| Version    |  v0.01                                           |"
+echo "| License    |  BSD 2-Clause Simplified License                 |"
+echo "+------------+--------------+-----------------------------------+"
+sleep 2 &&
+
 pacman -Syy git --noconfirm &&
 
 if [[ -d "/install" ]];then
